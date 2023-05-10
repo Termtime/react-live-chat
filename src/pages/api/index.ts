d7c33e85971bc187fec8a5b5b6056f06bef5b094
Mario Mejia <mariofermejia99@gmail.com>
2023-05-09 23:23:06 -0600

    chore: update cors

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -70,7 +70,7 @@ export default async function handler(
   await NextCors(req, res, {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "localhost:3000",
+    origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
